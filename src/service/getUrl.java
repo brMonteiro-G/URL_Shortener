ed23f6f85d1ea1e95c61f743adb40cb21a32b843
brMonteiro-G <brgabriel.monteiro@gmail.com>
2023-09-06 00:43:13 -0300

    feat: Hello world from Lambda

---
+++ src/service/getUrl.java
@@ -0,0 +1,8 @@
+package src.service.getUrl
+
+public class LambdaMethodHandler {
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log("Input: " + input);
+        return "Hello World - " + input;
+    }
+}
